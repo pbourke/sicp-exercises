fa0f046f62093ab074513f3d9ef16317eb697f41
Patrick Bourke <pbourke@gmail.com>
2012-08-13 09:47:55 -0700

    exercise 1.3

---
+++ ch1/exercises.rkt
@@ -0,0 +1,88 @@
+;1.1
+10
+(+ 5 4 3)
+(- 9 1)
+(/ 6 2)
+(+ (* 2 4) (- 4 6))
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b))
+(= a b)
+(if (and (> b a) (< b (* b a)))
+    b
+    a)
+
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+
+(+ 2 (if (> b a) b a))
+
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))
+
+;1.2
+(/ (+ 5
+      4
+      (- 2
+         (- 3 
+            (+ 6 (/ 4 5)))))
+   (* 3
+      (- 6 2)
+      (- 2 7)))
+
+;Exercise 1.3: Define a procedure that takes three numbers 
+;as arguments and returns the sum of the squares of the two larger numbers.
+;x,y  - x >= z && y >= z 
+;x,z  - x >= y && z >= y
+;y,z  - y >= x && z >= x
+(define (largest-two a b c)
+  (cond ((and 
+          (>= a c) 
+          (>= b c)) (list a b))
+        ((and 
+          (>= a b) 
+          (>= c b)) (list a c))
+        (else (list b c))))
+
+(largest-two 1 2 3)
+(largest-two 1 1 1)
+(largest-two 3 2 1)
+(largest-two 2 3 1)
+(largest-two 2 1 3)
+(largest-two 2 1 1)
+(largest-two 1 2 1)
+(largest-two 1 1 2)
+
+(define (sum-of-squares x y)
+  (+ (* x x)
+     (* y y)))
+
+(sum-of-squares  1 2)
+
+; solution without using apply:
+(define (sum-of-squares-of-largest-two a b c)
+  (cond ((and 
+          (>= a c) 
+          (>= b c)) (sum-of-squares a b))
+        ((and 
+          (>= a b) 
+          (>= c b)) (sum-of-squares a c))
+        (else (sum-of-squares b c))))
+
+(sum-of-squares-of-largest-two 1 2 3)
+(sum-of-squares-of-largest-two 1 1 1)
+(sum-of-squares-of-largest-two 1 2 1)
+(sum-of-squares-of-largest-two 2 3 1)
+
+; solution with apply
+(define (sum-of-squares-of-largest-two-apply a b c)
+  (apply sum-of-squares (largest-two a b c)))
+
+(sum-of-squares-of-largest-two-apply 1 2 3)
+(sum-of-squares-of-largest-two-apply 1 1 1)
+(sum-of-squares-of-largest-two-apply 1 2 1)
+(sum-of-squares-of-largest-two-apply 2 3 1)
+  
